8cc6e51895d0cc3cfa337ab93f70f7c4569f48cc
Aravind Kannan Rathinasabapathi <55327432+Aravind-Kannan@users.noreply.github.com>
2026-06-18 08:05:04 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet();
+        for(int num: nums) 
+        {
+            s.add(num);
+        }
+        return s.size() != nums.length;
+    }
+}
