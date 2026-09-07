aa70490e29296ac17c86f72b4a93236f24521c40
Aravind Kannan Rathinasabapathi <55327432+Aravind-Kannan@users.noreply.github.com>
2026-09-07 21:45:27 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    int count(int n) {
+        int count = 0;
+        while(n > 0)
+        {
+            count += (n & 1);
+            n >>= 1;
+        }
+        return count;
+    }
+
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for(int i = 0; i <= n; i++)
+        {
+            result[i] = count(i);
+        }
+        return result;
+    }
+}
